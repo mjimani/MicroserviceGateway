2da1c462af0ff763e69591cc769d68da69ba312f
MjImani <mjimani.ir@gmail.com>
2021-06-14 19:46:55 +0430

    add config server

--- src/main/java/ir/mjimani/gateway/GatewayApplication.java
+++ src/main/java/ir/mjimani/gateway/GatewayApplication.java
@@ -2,8 +2,6 @@ package ir.mjimani.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 public class GatewayApplication {
